15bb666a94fbdd870b214e18043094d7a63560f9
Om Mishra <contact@om-mishra.com>
2025-01-04 13:48:36 +0000

    Log special login events for ChaiWithCode in MongoDB

--- backend/main.py
+++ backend/main.py
@@ -1299,6 +1299,16 @@ def chai_with_code():
         session["user"]["user_profile"]["display_name"] = "ChaiAurCode"
         session["user"]["user_profile"]["avatar_url"] = "https://yt3.googleusercontent.com/1FEdfq3XpKE9UrkT4eOc5wLF2Bz-42sskTi0RkK4nPh4WqCbVmmrDZ5SVEV3WyvPdkfR8sw2=s160-c-k-c0x00ffffff-no-rj"
 
+        mongodb_client.logs.insert_one(
+            {
+                "log_id": str(uuid.uuid4()),
+                "log_type": "special_login",
+                "log_description": "Special login for ChaiWithCode",
+                "log_ip_address": request.headers.get("CF-Connecting-IP"),
+                "created_at": datetime.now(),
+            }
+        )
+
         return redirect(url_for("homepage"))
 
     return redirect(url_for("login"))
